550cb0328f4016c29c01c07b90da7afdec0492d4
mordenius <mordenius@gmail.com>
2019-10-30 17:32:15 +0200

    chore(UploadFactory) spec pass

--- src/ts/uploader/UploaderFactory.ts
+++ src/ts/uploader/UploaderFactory.ts
@@ -1,12 +1,15 @@
 import { Readable } from "stream";
 import { ImageUploader } from "./ImageUploader";
 import { VideoUploader } from "./VideoUploader";
+import { CONTENT_EXTENSION, IUploader } from "./_interfaces";
 
-export interface IUploader {
-	metadata: IMetadata;
-	saveOrigin(): Promise<void>;
-	createAndSaveThumbnail(): Promise<void>;
-}
+import { createWriteStream } from "fs";
+
+// export interface IUploader {
+// 	metadata: IMetadata;
+// 	saveOrigin(): Promise<void>;
+// 	createAndSaveThumbnail(): Promise<void>;
+// }
 
 export interface IMetadata {
 	width: number;
@@ -15,16 +18,23 @@ export interface IMetadata {
 }
 
 export interface IUploaderFactory {
-	getImageUploader(source: Readable, filename: string): IUploader;
-	getVideoUploader(source: Readable, filename: string): IUploader;
+	getImageUploader(source: Readable, filename: string, ext: CONTENT_EXTENSION): IUploader;
+	// getVideoUploader(source: Readable, filename: string, ext: CONTENT_EXTENSION): IUploader;
 }
 
 export class UploaderFactory implements IUploaderFactory {
-	public getImageUploader(source: Readable, filename: string): IUploader {
-		return new ImageUploader(source, filename);
+	public getImageUploader(source: Readable, filename: string, ext: CONTENT_EXTENSION): IUploader {
+		return async (source: Readable, filename: string, ext: CONTENT_EXTENSION): Promise<IMetadata> => {
+			const imageLoader = new ImageUploader(source, filename + "." + ext);
+
+			await imageLoader.saveOrigin();
+			// await imageLoader.createAndSaveThumbnail();
+
+			return imageLoader.metadata;
+		};
 	}
 
-	public getVideoUploader(source: Readable, filename: string): IUploader {
-		return new VideoUploader(source, filename);
-	}
+	// public getVideoUploader(source: Readable, filename: string, ext: CONTENT_EXTENSION): IUploader {
+	// 	// return new VideoUploader(source, filename);
+	// }
 }
--- src/ts/uploader/index.ts
+++ src/ts/uploader/index.ts
@@ -1,1 +1,21 @@
-export { UploaderFactory, IUploaderFactory } from "./UploaderFactory";
+import { Readable } from "stream";
+import { CONTENT_EXTENSION, IMetadata, IUploader } from "./_interfaces";
+import { UploaderFactory, IUploaderFactory } from "./UploaderFactory";
+
+export { CONTENT_EXTENSION, IMetadata, IUploader };
+
+const factory = new UploaderFactory();
+
+export function getUploader({ routes: { origin } }: { routes: { origin: string } }): IUploader {
+	return (source: Readable, name: string, ext: CONTENT_EXTENSION): Promise<IMetadata> => {
+		switch (ext) {
+			case CONTENT_EXTENSION.JPEG:
+				return factory.getImageUploader(source, name, ext)(source, name, ext);
+			case CONTENT_EXTENSION.MP4:
+				throw new Error(`Not implemented`);
+
+			default:
+				throw new Error(`Unsupported file extension`);
+		}
+	};
+}
