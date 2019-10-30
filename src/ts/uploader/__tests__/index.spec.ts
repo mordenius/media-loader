c1711d73e2d5a75dd4d20e040e2484bddd064c35
mordenius <mordenius@gmail.com>
2019-10-30 17:30:44 +0200

    spec(*) Image origin saves

--- src/ts/uploader/__tests__/index.spec.ts
+++ src/ts/uploader/__tests__/index.spec.ts
@@ -1,5 +1,61 @@
+import { createReadStream, existsSync } from "fs";
+import { CONTENT_EXTENSION, getUploader, IMetadata, IUploader } from "./..";
+
+const ZERO_DURATION = 0;
+const CONFIG = {
+	thumbnail: {
+		width: 120
+	},
+	routes: {
+		origin: `${process.cwd()}/temp/`,
+		thumbnail: `${process.cwd()}/temp/thumbnail/`
+	}
+};
+
 describe("Not implemented", (): void => {
-	it("No one test", (): void => {
-		throw new Error("Any test not exist");
+	const upload: IUploader = getUploader(CONFIG);
+
+	describe("Images", () => {
+		const source = createReadStream(process.cwd() + "/temp/1.jpeg");
+
+		const destinationName = "some-name";
+		const extension = CONTENT_EXTENSION.JPEG;
+
+		let metadata: IMetadata;
+
+		beforeAll((done: () => void): void => {
+			upload(source, destinationName, extension).then((meta: IMetadata): void => {
+				metadata = meta;
+				done();
+			});
+		});
+
+		it("returned value", (): void => {
+			expect(typeof metadata.width).toBe("number");
+			expect(typeof metadata.height).toBe("number");
+			expect(metadata.duration).toBe(ZERO_DURATION);
+		});
+
+		it("origin file is saved", (): void => {
+			const fileFullPath = CONFIG.routes.origin + destinationName + "." + extension;
+			const isExist: boolean = existsSync(fileFullPath);
+
+			expect(isExist).toBeTruthy();
+		});
+	});
+
+	describe("Video", (): void => {
+		// const source = createReadStream(process.cwd() + "/temp/1.mp4");
+		// upload(source, "some-name", CONTENT_EXTENSION.MP4)
+		// 	.then((meta: IMetadata): void => {
+		// 		console.log(`
+		// 	width: ${meta.width},
+		// 	height: ${meta.height},
+		// 	duration: ${meta.duration}
+		// `);
+		// 	})
+		// 	.catch((err: Error): void => {
+		// 		console.log(err.message);
+		// 	});
 	});
 });
